7e2279541eb9e98e634dd64aa5b311351fbc82e6
panenming <panenming@enn.cn>
2018-04-03 14:14:28 +0800

    3 sum closest

---
+++ 3sum-closest/3sum-closest.go
@@ -0,0 +1,38 @@
+package leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	res, delta := 0, math.MaxInt64
+	for i := range nums {
+		// 避免重复计算
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			s := nums[i] + nums[l] + nums[r]
+			switch {
+			case s < target:
+				l++
+				if delta > target-s {
+					delta = target - s
+					res = s
+				}
+			case s > target:
+				r--
+				if delta > s-target {
+					delta = s - target
+					res = s
+				}
+			default:
+				return s
+			}
+		}
+	}
+	return res
+}
