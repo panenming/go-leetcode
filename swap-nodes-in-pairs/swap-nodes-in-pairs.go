c7187934cbf7afad2e97aeaa72785b33c0755397
panenming <panenming@enn.cn>
2018-04-03 16:58:00 +0800

    swap nodes in pairs

---
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.go
@@ -0,0 +1,17 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	temp := head.Next
+	head.Next = swapPairs(temp.Next)
+	temp.Next = head
+	return temp
+}
