b345bd75bc5b8a89a6f965a194b87319607433e9
panenming <panenming@enn.cn>
2018-04-03 17:40:13 +0800

    impl strstr

---
+++ implement-strstr/implement-strstr.go
@@ -0,0 +1,11 @@
+package leetcode
+
+func strStr(haystack, needle string) int {
+	hlen, nlen := len(haystack), len(needle)
+	for i := 0; i <= hlen-nlen; i++ {
+		if haystack[i:i+nlen] == needle {
+			return i
+		}
+	}
+	return -1
+}
