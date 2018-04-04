946c2058a9f54b90fa61691b0fd66807386e7ba3
panenming <panenming@enn.cn>
2018-04-04 15:51:06 +0800

    search in rorated sorted array

---
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
@@ -0,0 +1,34 @@
+package leetcode
+
+func search(nums []int, target int) int {
+	var index, indexOfMax int
+	length := len(nums)
+
+	if length == 0 {
+		return -1
+	}
+
+	// 获取最大值的索引号，以便进行索引号变换
+	for indexOfMax+1 < length && nums[indexOfMax] < nums[indexOfMax+1] {
+		indexOfMax++
+	}
+
+	low, high, median := 0, length-1, 0
+	for low <= high {
+		median = (low + high) / 2
+		index = median + indexOfMax + 1
+		if index >= length {
+			index = index - length
+		}
+
+		switch {
+		case nums[index] > target:
+			high = median - 1
+		case nums[index] < target:
+			low = median + 1
+		default:
+			return index
+		}
+	}
+	return -1
+}
