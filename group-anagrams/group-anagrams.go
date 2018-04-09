d83e1ba9a2fc46610e94e60516f131fe83a60d7e
panenming <panenming@enn.cn>
2018-04-09 17:47:20 +0800

    group anagrams

---
+++ group-anagrams/group-anagrams.go
@@ -0,0 +1,39 @@
+package leetcode
+
+import (
+	"sort"
+)
+
+func groupAnagrams(strs []string) [][]string {
+	res := [][]string{}
+	record := make(map[string][]string)
+
+	for _, str := range strs {
+		temp := sortString(str)
+		record[temp] = append(record[temp], str)
+	}
+
+	for _, v := range record {
+		// 根据字符进行排序
+		sort.Strings(v)
+		res = append(res, v)
+	}
+	return res
+}
+
+func sortString(s string) string {
+	bytes := []byte(s)
+
+	temp := make([]int, len(bytes))
+	for i, b := range bytes {
+		temp[i] = int(b)
+	}
+
+	sort.Ints(temp)
+
+	for i, v := range temp {
+		bytes[i] = byte(v)
+	}
+
+	return string(bytes)
+}
