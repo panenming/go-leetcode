a428a9340327db8cfedeae22d5792fc70d36339b
panenming <panenming@enn.cn>
2018-04-10 18:37:40 +0800

    rm duplicates from sorted list

---
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,23 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	temp := head
+	for temp.Next != nil {
+		if temp.Val == temp.Next.Val {
+			temp.Next = temp.Next.Next
+		} else {
+			temp = temp.Next
+		}
+	}
+
+	return head
+}
