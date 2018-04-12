7c34c51f1e47682bbcf97a2b43fdf0a3364397a3
panenming <panenming@enn.cn>
2018-04-12 16:51:50 +0800

    subsets

---
+++ subsets/subsets.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import (
+	"sort"
+)
+
+func subsets(nums []int) [][]int {
+	res := [][]int{}
+	recur(nums, []int{}, &res)
+	return res
+}
+
+func recur(nums, temp []int, res *[][]int) {
+	l := len(nums)
+	if l == 0 {
+		sort.Ints(temp)
+		*res = append(*res, temp)
+		return
+	}
+
+	recur(nums[:l-1], temp, res)
+	recur(nums[:l-1], append([]int{nums[l-1]}, temp...), res)
+}
