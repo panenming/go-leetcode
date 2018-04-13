567901a5bd96fae3f27d057653285965e81122dc
panenming <panenming@enn.cn>
2018-04-13 16:14:41 +0800

    unique binary search trees

---
+++ unique-binary-search-trees/unique-binary-search-trees.go
@@ -0,0 +1,29 @@
+package leetcode
+
+func numTrees(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	if n == 1 || n == 2 {
+		return n
+	}
+
+	if n == 3 {
+		return 5
+	}
+
+	res := 0
+	// 左右对称，所以只做一半
+	for i := 1; i <= n/2; i++ {
+		res += numTrees(i-1) * numTrees(n-i)
+	}
+
+	res *= 2
+	if n%2 == 1 {
+		temp := numTrees(n / 2)
+		res += temp * temp
+	}
+
+	return res
+}
