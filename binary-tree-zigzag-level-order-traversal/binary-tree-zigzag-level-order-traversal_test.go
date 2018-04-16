f025372283fce181bf532eccde755ad5da5e9f3d
panenming <panenming@enn.cn>
2018-04-16 16:38:45 +0800

    binary-tree-zigzag-level-order-traversal

---
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal_test.go
@@ -0,0 +1,38 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/panenming/leetcode/datastruct"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_OK(t *testing.T) {
+	ast := assert.New(t)
+
+	// tcs is testcase slice
+	tcs := []struct {
+		pre, in []int
+		ans     [][]int
+	}{
+
+		{
+			[]int{3, 9, 20, 15, 7},
+			[]int{9, 3, 15, 20, 7},
+			[][]int{
+				[]int{3},
+				[]int{20, 9},
+				[]int{15, 7},
+			},
+		},
+
+		// 可以多个 testcase
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		ast.Equal(tc.ans, zigzagLevelOrder(datastruct.PreIn2Tree(tc.pre, tc.in)), "输入:%v", tc)
+	}
+}
