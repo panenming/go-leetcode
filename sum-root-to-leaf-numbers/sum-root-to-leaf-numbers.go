c2fc55a6d9963cfc13838740aef0763dd6710d13
panenming <panenming@enn.cn>
2018-04-19 17:55:09 +0800

    sum-root-to-leaf-numbers

---
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"github.com/panenming/leetcode/datastruct"
+)
+
+type TreeNode = datastruct.TreeNode
+
+func sumNumbers(root *TreeNode) int {
+	res := 0
+	var dfs func(*TreeNode, int)
+	dfs = func(root *TreeNode, temp int) {
+		if root == nil {
+			return
+		}
+
+		temp = temp*10 + root.Val
+
+		if root.Left == nil && root.Right == nil {
+			res += temp
+			return
+		}
+
+		dfs(root.Left, temp)
+		dfs(root.Right, temp)
+	}
+
+	dfs(root, 0)
+
+	return res
+}
---
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers_test.go
@@ -0,0 +1,52 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/panenming/leetcode/datastruct"
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	pre, in []int
+	ans     int
+}{
+
+	{
+		[]int{1, 2, 3, 4},
+		[]int{2, 1, 3, 4},
+		146,
+	},
+
+	{
+		[]int{1, 2, 3},
+		[]int{2, 1, 3},
+		25,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_OK(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		root := datastruct.PreIn2Tree(tc.pre, tc.in)
+		ast.Equal(tc.ans, sumNumbers(root), "输入:%v", tc)
+	}
+}
+
+func Benchmark_sumNumbers(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			b.StopTimer()
+			root := datastruct.PreIn2Tree(tc.pre, tc.in)
+			b.StartTimer()
+			sumNumbers(root)
+		}
+	}
+}
