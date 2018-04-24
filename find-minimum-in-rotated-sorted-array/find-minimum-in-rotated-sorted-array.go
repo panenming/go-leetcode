4ddb621b9e98da8a390345fe29cc4f808599486f
panenming <panenming@enn.cn>
2018-04-24 11:03:46 +0800

    find min dups

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -1,9 +1,33 @@
 package leetcode
-func findMin(a []int) int{
+
+func findMin(a []int) int {
 	len := len(a)
 	i := 1
-	for i < len && a[i-1] < a[i]{
+	for i < len && a[i-1] < a[i] {
 		i++
 	}
 	return a[i%len]
+}
+
+// 存在重复值
+func findMinDups(a []int) int {
+	l := len(a)
+	if a[0] < a[l-1] {
+		return a[0]
+	}
+
+	i, j := 0, l-1
+	for i < j {
+		if a[i] > a[i+1] {
+			return a[i+1]
+		}
+
+		if a[j-1] > a[j] {
+			return a[j]
+		}
+
+		i++
+		j--
+	}
+	return a[i]
 }
