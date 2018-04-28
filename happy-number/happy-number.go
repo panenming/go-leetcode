99670868f35a9350147c01c43f06460472891c80
panenming <panenming@enn.cn>
2018-04-28 14:58:32 +0800

    happy-number

---
+++ happy-number/happy-number.go
@@ -0,0 +1,37 @@
+package leetcode
+
+func isHappy(n int) bool {
+	slow, fast := n, trans(n)
+	for slow != fast {
+		slow = trans(slow)
+		fast = trans(trans(fast))
+	}
+	if slow == 1 {
+		return true
+	}
+	return false
+}
+
+func isHappy2(n int) bool {
+	result := make(map[int]bool)
+	for {
+		n = trans(n)
+		if result[n] {
+			break
+		}
+		result[n] = true
+	}
+	if n == 1 {
+		return true
+	}
+	return false
+}
+
+func trans(n int) int {
+	res := 0
+	for n != 0 {
+		res += (n % 10) * (n % 10)
+		n /= 10
+	}
+	return res
+}
